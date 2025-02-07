e26e3e3570135ab7747948b4f18ffbf80fa854bc
Akshaynitian <akshaychaman2004@gmail.com>
2025-02-07 21:28:40 +0530

    update

--- IMP_TOPICS/Intervals/Divide_Intervals_Into_Minimum_Number_of_Groups.cpp
+++ IMP_TOPICS/Intervals/Divide_Intervals_Into_Minimum_Number_of_Groups.cpp
@@ -5,55 +5,56 @@
 using namespace std;
 /* https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/ */
 class Solution {
-    public:
-        int minGroups(vector<vector<int>>& intervals) {
-            vector<int> start_times, end_times;
-    
-            // Extract start and end times
-            for (const auto& interval : intervals) {
-                start_times.push_back(interval[0]);
-                end_times.push_back(interval[1]);
-            }
-    
-            // Sort start and end times
-            sort(start_times.begin(), start_times.end());
-            sort(end_times.begin(), end_times.end());
-    
-            int end_ptr = 0, group_count = 0;
-    
-            // Traverse through the start times
-            for (int start : start_times) {
-                if (start > end_times[end_ptr]) end_ptr++; 
-                else group_count++;
-            }
-            return group_count;
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<int> start_times, end_times;
+
+        // Extract start and end times
+        for (const auto& interval : intervals) {
+            start_times.push_back(interval[0]);
+            end_times.push_back(interval[1]);
         }
+
+        // Sort start and end times
+        sort(start_times.begin(), start_times.end());
+        sort(end_times.begin(), end_times.end());
+
+        int end_ptr = 0, group_count = 0;
+
+        // Traverse through the start times
+        for (int start : start_times) {
+            if (start > end_times[end_ptr]) end_ptr++; 
+            else group_count++;
+        }
+        return group_count;
+    }
 };
 
 
 //Approach (Simply sorting and using heap)
 //T.C : O(nlogn)
 //S.C : O(n)
-/* class Solution {
-    public:
-        int minGroups(vector<vector<int>>& intervals) {
-            sort(begin(intervals), end(intervals));
-    
-            priority_queue<int, vector<int>, greater<int>> pq; //stores end point of groups
-    
-            for(vector<int>& interval : intervals) {
-                int start = interval[0];
-                int end   = interval[1];
-    
-                if(!pq.empty() && pq.top() < start) {
-                    pq.pop();
-                }
-                pq.push(end);
+/* 
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(begin(intervals), end(intervals));
+
+        priority_queue<int, vector<int>, greater<int>> pq; //stores end point of groups
+
+        for(vector<int>& interval : intervals) {
+            int start = interval[0];
+            int end   = interval[1];
+
+            if(!pq.empty() && pq.top() < start) {
+                pq.pop();
             }
-    
-            return pq.size();
+            pq.push(end);
         }
-    }; */
+
+        return pq.size();
+    }
+}; */
 int main(){
 return 0;
 }
