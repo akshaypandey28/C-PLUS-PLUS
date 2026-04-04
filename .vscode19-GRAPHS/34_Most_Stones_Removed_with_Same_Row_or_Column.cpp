715867e74cfe51d441958c3ec0181f57dd73a2e2
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-05 00:02:13 +0530

    from rank based to only parent based

--- .vscode19-GRAPHS/34_Most_Stones_Removed_with_Same_Row_or_Column.cpp
+++ .vscode19-GRAPHS/34_Most_Stones_Removed_with_Same_Row_or_Column.cpp
@@ -51,7 +51,6 @@ public:
     }
 };
 
-//2nd solution using rank
 class Solution {
 public:
     // DSU to find the number of connected component
@@ -59,25 +58,18 @@ public:
     // as in every connected component every node gets destroyed except one node which is not part of the answer
     // so basically after these steps every connected component has 1 node if 5 connected component then every has 1, total 5 nodes remain undestroyed
 
-    int find(vector<int> &parent, vector<int> &rank, int x) {
+    int find(vector<int> &parent,int x) {
         // TX:(log*n);
         // This method returns which group/cluster x belongs to
         if (parent[x] == x) return x;
-        return parent[x] = find(parent, rank, parent[x]);
+        return parent[x] = find(parent, parent[x]);
     }
 
-    void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
+    void Union(vector<int> &parent,int a, int b) {
         // TX:(log*n);
-        a = find(parent, rank, a);
-        b = find(parent, rank, b);
-        if (a == b) return;
-        if (rank[a] >= rank[b]) {
-            rank[a]++;
-            parent[b] = a;
-        } else {
-            rank[b]++;
-            parent[a] = b;
-        }
+        a = find(parent, a);
+        b=find(parent,b);
+        parent[b] = a;
     }
 
     int removeStones(vector<vector<int>>& stones) {
@@ -88,21 +80,23 @@ public:
         }
         int n = maxR + maxC + 2; // Ensure enough space for all rows and columns
         vector<int> parent(n);
-        vector<int> rank(n, 0);
-        for (int i = 0; i < n; ++i) parent[i] = i;
+        for (int i = 0; i < n; i++) parent[i] = i;
 
+        vector<bool> hasStone(n, false);
         for (auto ele : stones) {
             int nodeR = ele[0];
             int nodeC = ele[1] + maxR + 1; // for nodeColumn=row+1+col and col is ele[1]
-            Union(parent, rank, nodeR, nodeC);
+            Union(parent,nodeR, nodeC);
+            hasStone[nodeR] = true;
+            hasStone[nodeC] = true;
         }
 
         int count = 0;
         for (int i = 0; i < n; ++i) {
-            if (parent[i] == i and rank[i]>0) { //this ensures that at any ith node if rank is zero then it not part of our answer and rank[i]>0 means at least one node has a parent[i]==i which helps for answer
-                count++;
-            }
+            int parent_node=find(parent,i);
+            if ( hasStone[i] and parent_node == i) count++;
         }
+
         return stones.size() - count;
     }
 };
